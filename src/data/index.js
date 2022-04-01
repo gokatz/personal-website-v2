1b843235232cb5d7d1a1d642f75fcfacbfe71656
gokatz <gokulkathirvel@live.com>
2022-04-01 12:46:40 +0530

    make site deployable

---
+++ src/data/index.js
@@ -0,0 +1,9 @@
+export default {
+  siteName: 'Gokul Kathirvel',
+  disqusSiteName: 'gokatz',
+  siteDescription: `
+    A fellow human 🤗 love building things over web 💕 Doing React at Coinbase. 
+    Passionate about Web performance and Ember. Gokul Kathirvel writes blogs 
+    on Ember, Reatc, Chrome extension, Performance etc.`,
+  permalink: 'https://gokatz.me/'
+}
