09ba92223163c58920a74b80f71950834c7c1153
gokatz <gokulkathirvel@live.com>
2022-04-01 13:38:47 +0530

    adds GA

--- src/data/index.js
+++ src/data/index.js
@@ -5,5 +5,6 @@ export default {
     A fellow human 🤗 love building things over web 💕 Doing React at Coinbase. 
     Passionate about Web performance and Ember. Gokul Kathirvel writes blogs 
     on Ember, Reatc, Chrome extension, Performance etc.`,
-  permalink: 'https://gokatz.me/'
+  permalink: 'https://gokatz.me/',
+  gaId: 'UA-104177053-2'
 }
